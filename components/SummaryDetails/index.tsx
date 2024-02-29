9a05e2ecf4d2a82b1435957de03bcf0bc55f3edc
Josip Miljak <miljak.josip@outlook.com>
2024-03-01 00:22:10 +0100

    Styling summary page

--- components/SummaryDetails/index.tsx
+++ components/SummaryDetails/index.tsx
@@ -1,123 +1,104 @@
-"use client"; // Assuming you might need client-side rendering for other reasons
-
 import { useContext, useEffect, useState, useCallback, useMemo } from "react";
-import { Topic } from "../../types/Topic";
 import { DataContext } from "../../contexts/DataContext";
-import { TopicComments } from "../../types/TopicComments";
 import { SummaryContext } from "../../contexts/SummaryData";
-import { Summary } from "../../types/Summary";
-import { Card, CardBody, Divider } from "@nextui-org/react";
 import { SelectedSummaryTopicContext } from "../../contexts/SelectedSummaryJson";
+import { Topic } from "../../types/Topic";
+import { TopicComments } from "../../types/TopicComments";
+import { Summary } from "../../types/Summary";
 
 const SummaryDetails = () => {
-  const topicData = useContext(DataContext);
   const summaryData = useContext(SummaryContext);
-  const [filteredTopicData, setTopicData] = useState<TopicComments[]>([]);
+  const jsonTopicData = useContext(DataContext);
   const [summary, setSummary] = useState<Summary[]>([]);
-
+  const [topicData, setTopicData] = useState<TopicComments[]>([]);
   const { dataTopic, dataSubTopic } = useContext(SelectedSummaryTopicContext);
-
-  console.log("dataTopic... ", dataTopic);
-  console.log("dataSubTopic... ", dataSubTopic);
 
   useEffect(() => {
     const handleDataSet = () => {
       switch (dataTopic) {
         case Topic.OPPORTUNITIES:
           setSummary(summaryData.opportunitySummary);
+          setTopicData(jsonTopicData.opportunityData);
           break;
         case Topic.OPERATIONS:
           setSummary(summaryData.operationSummary);
+          setTopicData(jsonTopicData.operationData);
           break;
         case Topic.LEARNINGS:
           setSummary(summaryData.learningsSummary);
+          setTopicData(jsonTopicData.learningsData);
           break;
         case Topic.GAPS:
           setSummary(summaryData.gapsSummary);
+          setTopicData(jsonTopicData.gapsData);
       }
     };
     handleDataSet();
-  }, [
-    dataTopic,
-    summaryData.gapsSummary,
-    summaryData.learningsSummary,
-    summaryData.operationSummary,
-    summaryData.opportunitySummary,
-  ]);
+  }, [dataTopic, jsonTopicData, summaryData]);
 
-  console.log("SUMMARY... ", summary);
-
-  // const subTopic = "Sustainability Training";
-  // const topic = Topic.OPPORTUNITIES; // Are these dynamic from query parameters?
-
-  // Memoize the filtering logic
   const filterTopicDataBySubtopic = useCallback(() => {
-    return filteredTopicData.filter((item) => item.subtopic === dataSubTopic);
-  }, [filteredTopicData, dataSubTopic]);
+    return topicData.filter((item) => item.subtopic === dataSubTopic);
+  }, [topicData, dataSubTopic]);
 
-  // Memoize the filtered result
   const filteredTopicDataBySubtopicData = useMemo(filterTopicDataBySubtopic, [
     filterTopicDataBySubtopic,
   ]);
 
-  // Memoize the filtering logic
   const filterSubtopic = useCallback(() => {
     return summary.filter((item) => item.subtopic === dataSubTopic);
   }, [summary, dataSubTopic]);
-  // Memoize the filtered result
+
   const filteredSubtopicData = useMemo(filterSubtopic, [filterSubtopic]);
+  const filteredSingleSubtopicData = filteredSubtopicData.length
+    ? filteredSubtopicData[0].summary.split(".")
+    : [];
+
+  console.log("filteredSingleSubtopicData   ", filteredSingleSubtopicData);
 
   return (
-    <div className="flex w-fill flex-col">
+    <div className="flex flex-col w-full">
       {/* SUMMARY */}
-      <h1>Summary</h1>
-      <Card>
-        <div className="flex">
-          <CardBody>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                height: "100%",
-              }}
-            >
-              <p className="p-3 font-serif" style={{ margin: "auto" }}>
-                {filteredSubtopicData.length && filteredSubtopicData[0].summary}
+      <div className="bg-white shadow rounded-lg p-4 m-2 w-5/6">
+        <h1 className="text-3xl font-bold px-3 text-indigo-600 tracking-wide">
+          Summary
+        </h1>
+        <div className="flex flex-col justify-start h-full">
+          <ul className="list-disc list-inside text-base font-sans p-3">
+            {filteredSingleSubtopicData.map(
+              (point, index) =>
+                point.trim() !== "" && (
+                  <li key={index} className="text-left">
+                    {point}
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </div>
+
+      <hr className="my-4" />
+      {/* COMMENTS LIST */}
+      <h1 className=" px-9 text-3xl font-bold text-indigo-600 tracking-wide">
+        Comments
+      </h1>
+      <div className="grid grid-cols-3 gap-4">
+        {filteredTopicDataBySubtopicData.map((item) => (
+          <div
+            key={item.ID}
+            className="bg-white shadow rounded-lg p-4 m-2 flex flex-col justify-between h-full"
+          >
+            <div className="flex flex-col">
+              <p className="text-sm px-3 text-gray-500">
+                Country: {item.country}
+              </p>
+              <p className="text-base font-sans mx-auto p-3">
+                {item.comments_eng}
               </p>
             </div>
-          </CardBody>
-        </div>
-      </Card>
-      <Divider className="my-4" />
-      {/* COMMENTS LIST */}
-      <h1>Comments</h1>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "16px",
-        }}
-      >
-        {filteredTopicDataBySubtopicData.map((item) => (
-          <Card key={item.ID}>
-            <div className="flex">
-              <CardBody>
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    height: "100%",
-                  }}
-                >
-                  <p className="p-3 font-serif" style={{ margin: "auto" }}>
-                    {item.comments_eng}{" "}
-                  </p>
-                </div>
-              </CardBody>
-            </div>
-          </Card>
+            <p className="text-sm text-gray-500 px-3 py-1">
+              Posted on: {item.received}
+            </p>
+          </div>
         ))}
       </div>
     </div>
